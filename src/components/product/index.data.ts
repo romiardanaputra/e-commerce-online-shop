b8f2f7d8e265354a86e8bc2b2f2bdc768a237d7e
romiardanaputra <romiardana21@gmail.com>
2024-08-20 22:20:10 +0800

    feat(data): Add dummy data for simulate the components

---
+++ src/components/product/index.data.ts
@@ -0,0 +1,42 @@
+export const products = [
+  {
+    id: 1,
+    name: 'Apple Watch 4',
+    price: 399
+  },
+  {
+    id: 2,
+    name: 'Apple Watch 5',
+    price: 599
+  },
+  {
+    id: 3,
+    name: 'Apple Watch 6',
+    price: 799
+  },
+  {
+    id: 4,
+    name: 'Apple Watch 7',
+    price: 999
+  },
+  {
+    id: 5,
+    name: 'Apple Watch 8',
+    price: 1199
+  },
+  {
+    id: 6,
+    name: 'Apple Watch 9',
+    price: 1399
+  },
+  {
+    id: 7,
+    name: 'Apple Watch 10',
+    price: 1599
+  },
+  {
+    id: 8,
+    name: 'Apple Watch 11',
+    price: 1799
+  }
+]

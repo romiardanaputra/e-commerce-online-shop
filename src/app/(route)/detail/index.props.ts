19cce154bef5f1141e90f7d5947465b1f1ef71ac
romiardanaputra <romiardana21@gmail.com>
2024-08-22 20:17:01 +0800

    feat: Add type definition for productData and productContent

---
+++ src/app/(route)/detail/index.props.ts
@@ -0,0 +1,12 @@
+export type productDataProps = {
+  id: number
+  name: string
+  img: string
+}
+
+export type productDataContentProps = {
+  name: string
+  owner: string
+  price: string
+  description: string
+}

a7266ff00b27c72c60920995d84a7ce0a9ee3757
romiardanaputra <romiardana21@gmail.com>
2024-08-24 18:07:16 +0800

    feat: Add props and dummy data at landing

---
+++ src/app/index.data.ts
@@ -0,0 +1,83 @@
+export const landingCardData = [
+  {
+    id: 1,
+    name: 'Apple Watch',
+    img: 'apple-watch',
+    price: '399'
+  },
+  {
+    id: 2,
+    name: 'Black Edition Nike',
+    img: 'black-edition-nike',
+    price: '399'
+  },
+  {
+    id: 3,
+    name: 'Bubuk Maketi',
+    img: 'bubuk-maketi',
+    price: '399'
+  },
+  {
+    id: 4,
+    name: 'Mavic Kawe',
+    img: 'mavic-kawe',
+    price: '399'
+  },
+  {
+    id: 5,
+    name: 'Monkeys Toys',
+    img: 'monkey-toys',
+    price: '399'
+  },
+  {
+    id: 6,
+    name: 'Orange Bogota',
+    img: 'orange-bogota',
+    price: '399'
+  },
+  {
+    id: 7,
+    name: 'sofa-ternyaman',
+    img: 'sofa-ternyaman',
+    price: '399'
+  },
+  {
+    id: 8,
+    name: 'tatakan-gelas',
+    img: 'tatakan-gelas',
+    price: '399'
+  }
+]
+
+export const landingCategoryData = [
+  {
+    id: 1,
+    name: 'Baby',
+    img: 'baby'
+  },
+  {
+    id: 2,
+    name: 'Furniture',
+    img: 'furniture'
+  },
+  {
+    id: 3,
+    name: 'Gadget',
+    img: 'gadget'
+  },
+  {
+    id: 4,
+    name: 'Make Up',
+    img: 'make-up'
+  },
+  {
+    id: 5,
+    name: 'Sneaker',
+    img: 'sneaker'
+  },
+  {
+    id: 6,
+    name: 'Tools',
+    img: 'tools'
+  }
+]
---
+++ src/app/index.props.ts
@@ -0,0 +1,12 @@
+export type landingCategoryDataProps = {
+  id: number
+  name: string
+  img: string
+}
+
+export type landingCardDataProps = {
+  id: number
+  name: string
+  price: string
+  img: string
+}

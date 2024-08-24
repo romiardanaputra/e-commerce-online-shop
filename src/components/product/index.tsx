900b70141447b0b7c97c0a16f0664ca9fbb55178
romiardanaputra <romiardana21@gmail.com>
2024-08-24 18:17:16 +0800

    refactor(ui): Refine the code and called the dummy data for simulate the component

--- src/components/product/index.tsx
+++ src/components/product/index.tsx
@@ -2,35 +2,39 @@ import {
   Card,
   CardContent,
   CardDescription,
-  CardFooter,
   CardHeader,
   CardTitle
 } from '@/components/ui/card'
 import { products } from './index.data'
 import Image from 'next/image'
 import Link from 'next/link'
+import { landingCardDataProps } from '@/app/index.props'
 
-const ProductList = () => {
+type ProductListProps = {
+  landingCardData: landingCardDataProps[]
+}
+
+const ProductList = ({ landingCardData }: ProductListProps) => {
   return (
     <>
       <div className='px-6'>
-        <h1 className='mb-6 font-medium'>Product List</h1>
+        <h1 className='mb-6 text-xl font-medium'>Product List</h1>
 
         <div className='grid grid-cols-1 gap-4 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4'>
-          {products.map(product => (
+          {landingCardData.map(product => (
             <Link href={'/detail'} key={product.id}>
               <Card className='w-full max-[480px]:w-full'>
-                <CardHeader>
+                <CardContent className='p-6'>
                   <Image
-                    src={''}
-                    alt=''
+                    src={`/images/landing/landing-card-${product.img}.webp`}
+                    alt={product.name}
                     width={200}
                     height={200}
-                    className='h-40 w-full bg-neutral-500'
+                    className='mb-4 aspect-square h-40 w-full rounded-lg bg-neutral-500 object-cover sm:h-32 xl:h-40'
                   />
                   <CardTitle className='text-base'>{product.name}</CardTitle>
                   <CardDescription>${product.price}</CardDescription>
-                </CardHeader>
+                </CardContent>
               </Card>
             </Link>
           ))}

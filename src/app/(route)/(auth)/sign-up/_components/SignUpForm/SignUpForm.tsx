2adb0866a4b5dcdcc81312b7eacc7f09c88771ed
romiardanaputra <romiardana21@gmail.com>
2024-08-25 08:47:52 +0800

    feat(component): Add register form component

---
+++ src/app/(route)/(auth)/sign-up/_components/SignUpForm/SignUpForm.tsx
@@ -0,0 +1,127 @@
+'use client'
+
+import { useForm } from 'react-hook-form'
+import Link from 'next/link'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { z } from 'zod'
+
+import { Button } from '@/components/ui/button'
+import { Form } from '@/components/ui/form'
+import CustomField from '@/components/form/CustomField'
+import { FieldType } from '@/components/form/index.enum'
+
+const formSchema = z.object({
+  fullName: z.string().trim().min(2, {
+    message: 'Full Name must be at least 2 characters.'
+  }),
+  email: z.string().trim().toLowerCase().email({
+    message: 'Email must be valid.'
+  }),
+  password: z.string().min(6, {
+    message: 'Password must be at least 6 characters.'
+  }),
+  storeOpen: z.boolean(),
+  shopName: z.string().min(2, {
+    message: 'Shop Name must be at least 2 characters.'
+  }),
+  shopCategory: z.string({
+    message: 'Shop Category must be at least 2 characters.'
+  })
+})
+
+const SignUpForm = () => {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      fullName: '',
+      email: '',
+      password: '',
+      storeOpen: false,
+      shopName: '',
+      shopCategory: ''
+    }
+  })
+
+  const onSubmit = async (values: z.infer<typeof formSchema>) => {
+    console.log(values)
+  }
+  return (
+    <Form {...form}>
+      <form
+        onSubmit={form.handleSubmit(onSubmit)}
+        className='container max-w-screen-sm space-y-8 pt-12 xl:pt-20'
+      >
+        <h1 className='mb-5 text-3xl font-medium'>Start To Buy With New Way</h1>
+        <CustomField
+          control={form.control}
+          fieldName='fullName'
+          label='Fullname'
+          description='ex fullname: Kadek Romi Ardana Putra'
+          placeholder='fullname...'
+          fieldType={FieldType.INPUT}
+        />
+
+        <CustomField
+          control={form.control}
+          fieldName='email'
+          label='Email Address'
+          description='ex: romiardana@gmail.com'
+          placeholder='email...'
+          fieldType={FieldType.INPUT}
+        />
+
+        <CustomField
+          control={form.control}
+          fieldName='password'
+          label='Password'
+          description='we suggest that the password contains capital letter, number, symbols'
+          placeholder='password...'
+          fieldType={FieldType.INPUT}
+        />
+        <CustomField
+          control={form.control}
+          fieldType={FieldType.RADIO}
+          fieldName='storeOpen'
+          label='Store'
+          description='want to open store?'
+          placeholder=''
+        />
+
+        <CustomField
+          control={form.control}
+          fieldName='shopName'
+          label='Shop Name'
+          description='your shopname will be displayed on your store'
+          placeholder='shopname...'
+          fieldType={FieldType.INPUT}
+        />
+
+        <CustomField
+          control={form.control}
+          fieldName='shopCategory'
+          label='Category'
+          description='Choose you shop category'
+          placeholder='shop category...'
+          fieldType={FieldType.SELECT}
+        />
+        <div className='flex flex-col gap-4'>
+          <Button type='submit' size={'lg'}>
+            Submit
+          </Button>
+          <Link href='/login'>
+            <Button
+              type='submit'
+              variant={'outline'}
+              size={'lg'}
+              className='w-full'
+            >
+              Back to Sign In
+            </Button>
+          </Link>
+        </div>
+      </form>
+    </Form>
+  )
+}
+export default SignUpForm

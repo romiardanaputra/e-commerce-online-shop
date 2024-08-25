02f683d7b08d30fe86ce36271fa497068a6e2d40
romiardanaputra <romiardana21@gmail.com>
2024-08-25 08:45:44 +0800

    feat(component): Add login form component

---
+++ src/app/(route)/(auth)/login/_components/LoginForm/LoginForm.tsx
@@ -0,0 +1,84 @@
+'use client'
+
+import { zodResolver } from '@hookform/resolvers/zod'
+import { useForm } from 'react-hook-form'
+import { z } from 'zod'
+
+import { Form } from '@/components/ui/form'
+import CustomField from '@/components/form/CustomField'
+import { FieldType } from '@/components/form/index.enum'
+import { Button } from '@/components/ui/button'
+import Link from 'next/link'
+
+const loginFormSchema = z.object({
+  email: z.string().trim().email({
+    message: 'Email must be valid.'
+  }),
+  password: z.string().trim().min(6, {
+    message: 'Password must be at least 6 characters.'
+  })
+})
+
+const LoginForm = () => {
+  const onLoginSubmit = async (values: z.infer<typeof loginFormSchema>) => {
+    console.log(values)
+  }
+
+  const form = useForm<z.infer<typeof loginFormSchema>>({
+    resolver: zodResolver(loginFormSchema),
+    defaultValues: {
+      email: '',
+      password: ''
+    }
+  })
+
+  return (
+    <>
+      <Form {...form}>
+        <form
+          className='container max-w-screen-sm space-y-8 pt-12 xl:pt-20'
+          onSubmit={form.handleSubmit(onLoginSubmit)}
+        >
+          <h1 className='text-3xl font-medium'>
+            Belanja kebutuhan utama, menjadi makin mudah
+          </h1>
+          <CustomField
+            control={form.control}
+            fieldName='email'
+            label='Email Address'
+            description='ex: romiardana21@gmail.com'
+            placeholder='email'
+            fieldType={FieldType.INPUT}
+          />
+
+          <CustomField
+            control={form.control}
+            fieldName='password'
+            label='Password'
+            description='we suggest that the password contains capital letter, number, symbols'
+            placeholder='password...'
+            fieldType={FieldType.INPUT}
+          />
+
+          <div className='flex flex-col gap-4'>
+            <Button type='submit' size={'lg'}>
+              Sign in to my account
+            </Button>
+            <Link href='/sign-up'>
+              <Button
+                type='submit'
+                variant={'outline'}
+                size={'lg'}
+                className='w-full'
+              >
+                Sign Up
+              </Button>
+            </Link>
+          </div>
+        </form>
+      </Form>
+    </>
+  )
+}
+
+export default LoginForm

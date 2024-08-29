d7004339a53b416d170ca23c3db6b338a7764ca9
romiardanaputra <romiardana21@gmail.com>
2024-08-29 18:40:44 +0800

    refactor(route): Group all files and folder into user's route folder

---
+++ src/app/(route)/(user)/transaction-success/_components/TransactionSuccess/index.tsx
@@ -0,0 +1,36 @@
+import { Button } from '@/components/ui/button'
+import Image from 'next/image'
+
+const TransactionSuccessComponent = () => {
+  return (
+    <>
+      <div className='container relative min-h-screen max-w-screen-sm'>
+        <div className='absolute left-1/2 top-1/2 flex w-4/5 -translate-x-1/2 -translate-y-1/2 flex-col items-center justify-center gap-4 sm:w-fit'>
+          <Image
+            src='/images/register/register-success-icon.webp'
+            alt='register icon success'
+            width={150}
+            height={150}
+            className='aspect-square object-cover object-center'
+          />
+          <section className='mb-8 space-y-4 text-center'>
+            <h4 className='text-3xl font-medium'>Transaction Proceed</h4>
+            <p>
+              Silahkan tunggu konfirmasi email dari kami dan kami akan
+              menginformasikan resi secept mungkin!
+            </p>
+          </section>
+
+          <div className='flex flex-col gap-4'>
+            <Button size={'lg'}>My Dashboard</Button>
+            <Button size={'lg'} variant={'outline'}>
+              Go to shopping
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
+
+export default TransactionSuccessComponent

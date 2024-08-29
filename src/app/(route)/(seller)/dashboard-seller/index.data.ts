e5aa9979ff21bba0d81e537c8757dce0033b59ba
Kadek Romi Ardana Putra <romiardana21@gmail.com>
2024-08-29 18:30:21 +0800

    [ADD]/DEV/FEATURE/Implement UI of dashboard seller (#7)
    * chore: Add shadcn ui components
    
    * chore: Delete unused files
    
    * refactor(layout): Refine app layout
    
    * feat(ui): Add shadcn UI components
    
    * chore: Enable discord presence
    
    * feat(data): Add dashboard seller's dummy data
    
    * feat(page): Add dashboard page for seller
    
    * feat(breadcrumb): Add breadcrumb component for dashboard seller
    
    * feat(card): Add card component for dashboard seller
    
    * feat(invoice): Add invoice component
    
    * feat(order): Add order component
    
    * feat(profile): Add profile component
    
    * feat(search bar): Add search bar component
    
    * feat(side nav): Add side nav desktop component
    
    * feat(side nav mobile): Add sidenav mobile component

---
+++ src/app/(route)/(seller)/dashboard-seller/index.data.ts
@@ -0,0 +1,45 @@
+export const tablists = [
+  {
+    value: 'week',
+    name: 'Week'
+  },
+  {
+    value: 'month',
+    name: 'month'
+  },
+  {
+    value: 'year',
+    name: 'Year'
+  }
+]
+
+export const orders = [
+  {
+    customer: 'Liam Johnson',
+    email: 'liam@example.com',
+    type: 'Sale',
+    status: 'Fulfilled',
+    amount: '$250.00'
+  },
+  {
+    customer: 'Olivia Smith',
+    email: 'olivia@example.com',
+    type: 'Refund',
+    status: 'Declined',
+    amount: '$150.00'
+  },
+  {
+    customer: 'Noah Williams',
+    email: 'noah@example.com',
+    type: 'Subscription',
+    status: 'Fulfilled',
+    amount: '$350.00'
+  },
+  {
+    customer: 'Emma Brown',
+    email: 'emma@example.com',
+    type: 'Sale',
+    status: 'Fulfilled',
+    amount: '$450.00'
+  }
+]

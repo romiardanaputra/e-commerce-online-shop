21156f5de6e93df4c021b24cdb0f564a6f86c339
romiardanaputra <romiardana21@gmail.com>
2024-08-30 23:13:18 +0800

    feat(data): Add data for seller product

---
+++ src/app/(route)/seller/products/index.data.ts
@@ -0,0 +1,105 @@
+export const sellerProducts = [
+  {
+    id: 1,
+    name: 'Apple Watch 4',
+    img: 'apple-watch',
+    price: '399',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 2,
+    name: 'Apple Watch 5',
+    img: 'apple-watch',
+    price: '599',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 3,
+    name: 'Apple Watch 6',
+    img: 'apple-watch',
+    price: '799',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 4,
+    name: 'Apple Watch 7',
+    img: 'apple-watch',
+    price: '999',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 5,
+    name: 'Apple Watch 7',
+    img: 'apple-watch',
+    price: '999',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 6,
+    name: 'Apple Watch 7',
+    img: 'apple-watch',
+    price: '999',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 7,
+    name: 'Apple Watch 7',
+    img: 'apple-watch',
+    price: '999',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  },
+  {
+    id: 8,
+    name: 'Apple Watch 7',
+    img: 'apple-watch',
+    price: '999',
+    status: 'Draft',
+    totalSale: 25,
+    createdAt: '2023-07-12 10:42 AM'
+  }
+]
+
+export const tableHeadDatas = [
+  {
+    id: 1,
+    name: 'Image'
+  },
+  {
+    id: 2,
+    name: 'Product'
+  },
+  {
+    id: 3,
+    name: 'Status'
+  },
+  {
+    id: 4,
+    name: 'Price'
+  },
+  {
+    id: 5,
+    name: 'Total Sales'
+  },
+  {
+    id: 6,
+    name: 'Created At'
+  },
+  {
+    id: 7,
+    name: 'Action'
+  }
+]

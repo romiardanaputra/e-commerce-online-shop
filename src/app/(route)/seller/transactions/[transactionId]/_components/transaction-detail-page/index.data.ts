96c7e9fe250ff5a04891cafa8d0f40c2601d8a08
romiardanaputra <romiardana21@gmail.com>
2024-09-03 13:46:59 +0800

    feat(data): Add data for transaction detail component

---
+++ src/app/(route)/seller/transactions/[transactionId]/_components/transaction-detail-page/index.data.ts
@@ -0,0 +1,32 @@
+export const transactionDetails = [
+  {
+    id: 1,
+    prop: 'Customer Name',
+    value: 'Romi Ardana'
+  },
+  {
+    id: 2,
+    prop: 'Date of Transaction',
+    value: '12 Januari, 2020'
+  },
+  {
+    id: 3,
+    prop: 'Total Amount',
+    value: '$280.408'
+  },
+  {
+    id: 4,
+    prop: 'Product Name',
+    value: 'sirup marjan'
+  },
+  {
+    id: 5,
+    prop: 'Payment Status',
+    value: 'pending'
+  },
+  {
+    id: 6,
+    prop: 'Phone Number',
+    value: '08123456789'
+  }
+]

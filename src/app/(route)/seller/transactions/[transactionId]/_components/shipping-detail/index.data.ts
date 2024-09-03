c5682c00c2b62e865361f226b30ea0cec0a5c197
romiardanaputra <romiardana21@gmail.com>
2024-09-03 13:46:06 +0800

    feat(data): Add data for shipping detail component

---
+++ src/app/(route)/seller/transactions/[transactionId]/_components/shipping-detail/index.data.ts
@@ -0,0 +1,37 @@
+export const shippingDetails = [
+  {
+    id: 1,
+    prop: 'Address',
+    value: 'Br pengubengan kangin PCP...'
+  },
+  {
+    id: 2,
+    prop: 'Address II',
+    value: 'Jl Merta Sari Asri...'
+  },
+  {
+    id: 3,
+    prop: 'Province',
+    value: 'Bali'
+  },
+  {
+    id: 4,
+    prop: 'City',
+    value: 'Denpasar'
+  },
+  {
+    id: 5,
+    prop: 'Postal Code',
+    value: '80361'
+  },
+  {
+    id: 7,
+    prop: 'Country',
+    value: 'indonesia'
+  },
+  {
+    id: 8,
+    prop: 'Shipping Status',
+    value: 'success'
+  }
+]

a1d79971d3807a51e60260c9a8642c6db3a53950
romiardanaputra <romiardana21@gmail.com>
2024-09-03 13:43:38 +0800

    feat(page): Add transactions page

--- src/app/(route)/seller/transactions/page.tsx
+++ src/app/(route)/seller/transactions/page.tsx
@@ -1,25 +1,7 @@
-import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
-import React from 'react'
-import SellProduct from './_components/sell-product'
-import BuyProduct from './_components/buy-product'
+import TransactionPageComponent from './_components/transaction-page'
 
 const TransactionPage = () => {
-  return (
-    <main className='p-4 sm:px-6 sm:py-0'>
-      <Tabs defaultValue='sellProduct' className='w-full'>
-        <TabsList className='grid w-3/12 grid-cols-2'>
-          <TabsTrigger value='sellProduct'>Sell Product</TabsTrigger>
-          <TabsTrigger value='buyProduct'>Buy Product</TabsTrigger>
-        </TabsList>
-        <TabsContent value='sellProduct'>
-          <SellProduct />
-        </TabsContent>
-        <TabsContent value='buyProduct'>
-          <BuyProduct />
-        </TabsContent>
-      </Tabs>
-    </main>
-  )
+  return <TransactionPageComponent />
 }
 
 export default TransactionPage
